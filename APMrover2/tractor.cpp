c5d3c408f42c4055e906e8af098e8af5aa9881ad
jose herrera <jherrera52@ucmerced.edu>
2018-03-23 14:33:42 -0700

    ardu

--- APMrover2/tractor.cpp
+++ APMrover2/tractor.cpp
@@ -1,12 +1,5 @@
-//
-//  tractor.cpp
-//  
-//
-//  Created by Jose Herrera on 3/7/18.
-//
-
-#include <stdio.h>
-#include <Rover.h>
+// -*- tab-width: 4, Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+#include "Rover.h"
 
 
 
@@ -18,14 +11,14 @@ void Rover::update_tractor_health(){
     for(int i = buff_len; i>=0; i--){
         if(5 > i){
             tractor_health_info[i] = hal.uartE->read();
-            hal.uartE->printf("i = %d tractor_health_info[i] = %c \n", i, tractor_health_info[i]);
+           // hal.uartE->printf("i = %d tractor_health_info[i] = %c \n", i, tractor_health_info[i]);
         }else{
             hal.uartE->read();
         }
     }
     
-    hal.uartE->printf("Read Tractor Health it is.....\n");
-    hal.uartE->printf(tractor_health_info);
+    //hal.uartE->printf("Read Tractor Health it is.....\n");
+    //hal.uartE->printf(tractor_health_info);
     
     
 }
